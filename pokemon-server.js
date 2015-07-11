202cf8402d346ca2f5d891a2a615e22e90689157
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2015-07-11 13:13:32 +0200

    add express server

--- pokemon-server.js
+++ pokemon-server.js
@@ -1,10 +1,21 @@
 /* eslint no-console:0 */
 
+import {PokemonSchema} from './pokemon-schema';
+import { graphql } from 'graphql';
+
 import express from 'express';
+import bodyParser from 'body-parser'
 
 const PORT = 8080;
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res, next) => {
+  const {query, pokemonId} = req.body;
+  graphql(PokemonSchema, query, null, {pokemonId}).then(res.send.bind(res));
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and running in port ${PORT}`);
